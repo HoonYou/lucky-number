811ec294420210d4075d2aed4a69b91f4ead5b01
HoonYou <epdgns@gmail.com>
2023-05-01 14:27:04 +0900

    typo

--- num-selector.py
+++ num-selector.py
@@ -4,7 +4,7 @@ import random as rand
 k=(input('how many times? : '))
 print(k)
 
-for j in range(k)
+for j in range(k):
     number=[]
 	for i in range(6):
    	    number.append(rand.randint(1,45))

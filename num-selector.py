048c2212964ab4dc446968061519690bccd6024b
HoonYou <epdgns@gmail.com>
2023-05-01 14:24:54 +0900

    add some details
    features : input

--- num-selector.py
+++ num-selector.py
@@ -1,7 +1,11 @@
 import numpy as np
 import random as rand
 
-number=[]
-for i in range(6):
-    number.append(rand.randint(1,45))
-print(number)
+k=(input('how many times? : '))
+print(k)
+
+for j in range(k)
+    number=[]
+	for i in range(6):
+   	    number.append(rand.randint(1,45))
+        print(number)
